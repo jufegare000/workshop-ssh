ca6af33d1639b117a176647478bd1f5f6eff59da
juan-gallo <juanfgallo@experimentality.co>
2025-05-23 18:00:23 -0500

    removing unnecessary step

--- 1-network-creation.sh
+++
@@ -1,8 +0,0 @@
-# Crear una red bridge
-nmcli connection add type bridge con-name host-custom-network ifname host-cstom-network
-
-# Asignar una IP estática (ajusta si es necesario)
-nmcli connection modify host-custom-network ipv4.addresses 192.168.100.1/24 ipv4.method manual
-
-# Activar la conexión
-nmcli connection up host-custom-network
